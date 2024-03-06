5a74902dd99c22d7342d6b164bd8395f95c4213b
Jelle Sjollema <j.sjollema@ma-web.nl>
2024-03-06 21:24:55 +0100

    les2

---
+++ Assets/Scripts/FromAToB.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FromAToB : MonoBehaviour
+{
+    [SerializeField] GameObject A;
+    [SerializeField] GameObject B;
+
+    [SerializeField] Vector3 differenceVector;
+    [SerializeField] float distance;
+
+    Vector3 direction;
+    [SerializeField] float speed = 2;
+
+    bool gotoB = true;
+
+    void Start()
+    {
+        transform.position = A.transform.position;
+    }
+
+    void Update()
+    {
+        if(gotoB)
+        {
+            differenceVector = B.transform.position - transform.position;
+            transform.LookAt(A.transform.position);
+        }
+        else
+        {
+            differenceVector = A.transform.position - transform.position;
+            transform.LookAt(B.transform.position);
+        }
+        
+        distance = differenceVector.magnitude;
+        direction = differenceVector.normalized;
+
+        transform.position += direction * speed * Time.deltaTime;
+
+        if(distance < speed*Time.deltaTime)
+        {
+            gotoB = !gotoB;
+        }
+    }
+}
