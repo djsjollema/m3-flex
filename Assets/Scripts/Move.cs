8039c18efba839e85e747f385810f40219bc5707
Jelle Sjollema <j.sjollema@ma-web.nl>
2024-03-06 14:10:26 +0100

    les2

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    Vector3 velocity = new Vector3(1, 1, 0);
+    Vector3 velocity = new Vector3(0.5f, 1, 0);
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("hallo yoenietie");
+        Debug.Log("halloooowww joenietie");
     }
+
     // Update is called once per frame
     void Update()
     {
-        transform.position += velocity * Time.deltaTime;
+        transform.position += velocity * 10 * Time.deltaTime;
+
         if(transform.position.y > 3 || transform.position.y < -3)
         {
             velocity = new Vector3(velocity.x, -velocity.y, 0);
         }
 
-        if (transform.position.x > 5 || transform.position.x < -5)
+        if(transform.position.x >5 || transform.position.x < -5)
         {
             velocity = new Vector3(-velocity.x, velocity.y, 0);
         }
